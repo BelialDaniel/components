0d4a26983058ef25e48237e862efff86bcce8614
Daniel Maldonado <belialdaniel@gmail.com>
2021-05-04 00:55:42 -0500

    First commit Vue app with typescript

---
+++ web/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <HelloWorld msg="Vue"/>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from './components/HelloWorld.vue';
+
+@Options({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style></style>
